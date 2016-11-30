3a61e73b9a4c71503241349dd93523a879f2b17d
Sam Easterby-Smith <me@samscam.co.uk>
2016-11-30 22:58:42 +0000

    Observe server status in settings and bind variable changes

--- Transmote/SettingsViewController.swift
+++ Transmote/SettingsViewController.swift
@@ -9,9 +9,12 @@
 import Foundation
 import Cocoa
 
+import RxSwift
+import RxCocoa
+
 class SettingsViewController: NSViewController {
     
-    var server: TransmissionServer?
+    var session: TransmissionSession?
     
     @IBOutlet weak var statusBlobImageView: NSImageView!
     
@@ -21,12 +24,58 @@ class SettingsViewController: NSViewController {
     @IBOutlet weak var usernameField: NSTextField!
     @IBOutlet weak var passwordField: NSSecureTextField!
     
+    var disposeBag: DisposeBag = DisposeBag()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        guard let session = session else {
+            return
+        }
+        
+        guard let server = session.server else {
+            return
+        }
+        
+        disposeBag = DisposeBag()
+        
+        
+        // Observe the session status
+        
+        session.status.asObservable().subscribe(onNext:{ status in
+            
+            switch status {
+            case .connected:
+                self.statusBlobImageView.image = NSImage(named: "status-green")
+            case .connecting:
+                self.statusBlobImageView.image = NSImage(named: "status-orange")
+            case .indeterminate:
+                self.statusBlobImageView.image = NSImage(named: "status-gray")
+            case .failed:
+                self.statusBlobImageView.image = NSImage(named: "status-red")
+            }
+            
+        }).addDisposableTo(disposeBag)
+        
+        // Initial server values
+        
+        serverAddressField.stringValue = server.address ?? ""
+        portField.stringValue = String(server.port)
+        rpcPathField.stringValue = server.rpcPath
+        
+
+        // Bind the fields back to the session
+
+        Observable.combineLatest(serverAddressField.rx.text, portField.rx.text, rpcPathField.rx.text){ ($0,$1,$2) }
+            .throttle(0.5, scheduler: MainScheduler.instance )
+            .subscribe(onNext:{(address,port,path) in
+                server.address = address
+                server.port = Int(port!) ?? 9091
+                server.rpcPath = path!
+                self.session?.connect()
+        }).addDisposableTo(disposeBag)
         
     }
-
     //    override func observeValue(forKeyPath keyPath: String, ofObject object: Any, change: [AnyHashable: Any], context: UnsafeMutableRawPointer) {
     //        if context == serverContext {
     //            if (keyPath == "connected") {
