1c87c8d663d88cc3e7f06583ebe60f99593f34d5
Sam Easterby-Smith <me@samscam.co.uk>
2016-12-01 21:28:01 +0000

    Show passive alert stuff on main view

--- Transmote/MainViewController.swift
+++ Transmote/MainViewController.swift
@@ -7,21 +7,64 @@
 //
 
 import Cocoa
+import RxSwift
+import Moya
 
 class MainViewController: NSViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+    var session: TransmissionSession? {
+        didSet{
+            bindToSession()
         }
     }
+    
+    var disposeBag: DisposeBag = DisposeBag()
+    
+    @IBOutlet weak var collectionView: NSCollectionView!
+    @IBOutlet weak var collectionViewContainer: NSScrollView!
+    
+    @IBOutlet weak var passiveAlertContainer: NSBox!
+    @IBOutlet weak var passiveAlertLabel: NSTextField!
+    @IBOutlet weak var passiveAlertImageView: NSImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bindToSession()
+    }
 
+    func bindToSession(){
+        
+        guard let session = session else {
+            return
+        }
 
+        guard self.isViewLoaded else {
+            return
+        }
+        disposeBag = DisposeBag()
+        
+        
+        // Observe the session status
+        
+        session.status.asObservable()
+            .debounce(0.2, scheduler: MainScheduler.instance)
+            .subscribe(onNext:{ status in
+                
+                switch status {
+                case .connected:
+                    self.passiveAlertContainer.isHidden = true
+                    self.collectionViewContainer.isHidden = false
+                case .connecting, .indeterminate:
+                    self.passiveAlertContainer.isHidden = false
+                    self.collectionViewContainer.isHidden = true
+                case .failed(let error):
+                    self.collectionViewContainer.isHidden = true
+                    self.passiveAlertContainer.isHidden = false
+                    self.passiveAlertLabel.stringValue = error.description
+
+                }
+                
+            }).addDisposableTo(disposeBag)
+    }
 }
 
