99ce44cb6cac8f4f54da79a5c55181a89bea6afd
Sam Easterby-Smith <me@samscam.co.uk>
2016-12-05 22:59:38 +0000

    Nah, hue adjust was better

--- Transmote/TorrentCollectionViewItem.swift
+++ Transmote/TorrentCollectionViewItem.swift
@@ -19,10 +19,12 @@ class TorrentCollectionViewItem: NSCollectionViewItem {
     @IBOutlet weak var progressStatusLabel: NSTextField!
     @IBOutlet weak var progressBar: NSProgressIndicator!
     
-    var hueFilter: CIFilter = CIFilter(name: "CIFalseColor" , withInputParameters:["inputColor0":CIColor(red:0,green:0,blue:0), "inputColor1":CIColor(red:1,green:1,blue:1)] )!
+//    var hueFilter: CIFilter = CIFilter(name: "CIFalseColor" , withInputParameters:["inputColor0":CIColor(red:0,green:0,blue:0), "inputColor1":CIColor(red:1,green:1,blue:1)] )!
+    var hueFilter: CIFilter = CIFilter(name: "CIHueAdjust" , withInputParameters:["inputAngle":0] )!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        progressBar.controlTint = .blueControlTint
         progressBar.contentFilters = [hueFilter]
     }
     var torrent: Torrent? { didSet {
@@ -34,27 +36,26 @@ class TorrentCollectionViewItem: NSCollectionViewItem {
             switch torrent.status{
             case .fetchingMetadata:
                 progressStatusLabel.stringValue = "Fetching metadata..."
-                hueFilter.setValue(CIColor(red:1,green:1,blue:1), forKey: "inputColor1")
+                hueFilter.setValue( Double.pi * 2 * 0.3 , forKey: "inputAngle")
             case .downloading:
-                hueFilter.setValue(CIColor(red:0,green:1,blue:0), forKey: "inputColor1")
+                hueFilter.setValue(Double.pi * 2 * 0, forKey: "inputAngle")
                 progressStatusLabel.stringValue = "Downloading"
             case .seeding:
-                hueFilter.setValue(CIColor(red:0,green:0,blue:1), forKey: "inputColor1")
+                hueFilter.setValue(Double.pi * 2 * 0.3, forKey: "inputAngle")
                 progressStatusLabel.stringValue = "Seeding"
             case .stalled:
-                hueFilter.setValue(CIColor(red:1,green:1,blue:1), forKey: "inputColor1")
+                hueFilter.setValue(Double.pi * 2 * 0.5, forKey: "inputAngle")
                 progressStatusLabel.stringValue = "Stalled"
             case .complete:
-                hueFilter.setValue(CIColor(red:0,green:1,blue:1), forKey: "inputColor1")
+                hueFilter.setValue(Double.pi * 2 * 0.65, forKey: "inputAngle")
                 progressStatusLabel.stringValue = "Complete"
             }
            
         } else {
-            hueFilter.setValue(CIColor(red:1,green:1,blue:1), forKey: "inputColor1")
+            progressStatusLabel.stringValue = ""
+            hueFilter.setValue(Double.pi * 2 * 0.3, forKey: "inputAngle")
         }
 
-        
         }
     }
-    
 }
