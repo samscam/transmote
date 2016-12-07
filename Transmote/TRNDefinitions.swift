ea31912e5b02fc959340e5f1c83fa1b483616680
Sam Easterby-Smith <me@samscam.co.uk>
2016-12-07 23:36:43 +0000

    Erm yes api key

--- Transmote/TRNDefinitions.swift
+++ Transmote/TRNDefinitions.swift
@@ -9,8 +9,9 @@
 //  For general configuration of things, URLs and API keys
 
 // The Movie Database
-let TMDB_API_KEY = "XXXX"
+let TMDB_API_KEY = "***REMOVED***"
 let TMDB_BASE_URL = "https://api.themoviedb.org/3/"
+let TMDB_IMAGES_URL = "https://image.tmdb.org/t/p/"
 
 let UPDATES_URL = "http://github.com/samscam/transmote"
 
