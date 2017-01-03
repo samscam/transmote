8993c8a8148222ee327900c47b33302fc719ef69
Sam Easterby-Smith <me@samscam.co.uk>
2017-01-03 16:10:25 +0000

    Lint - was ignoring this

--- Transmote/TRNDefinitions.swift
+++ Transmote/TRNDefinitions.swift
@@ -8,9 +8,10 @@
 //  For general configuration of things, URLs and API keys
 
 // The Movie Database
+
+// swiftlint:disable variable_name
 let TMDB_API_KEY = "XXX"
 let TMDB_BASE_URL = "https://api.themoviedb.org/3/"
 let TMDB_IMAGES_URL = "https://image.tmdb.org/t/p/"
 
 let UPDATES_URL = "http://github.com/samscam/transmote"
-
